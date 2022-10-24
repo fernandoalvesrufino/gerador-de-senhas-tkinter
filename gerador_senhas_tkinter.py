b2c84102623aeb8a587687b1feb2c11150fc6068
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-10-24 10:01:19 -0300

    Adicionado funcionalidade de copiar senha

--- gerador_senhas_tkinter.py
+++ gerador_senhas_tkinter.py
@@ -1,5 +1,8 @@
 from tkinter import *
-import random, string        
+import random
+import string
+import pyperclip as pc
+
 
 janela = Tk()
 janela.title('Gerador de Senhas')
@@ -8,30 +11,47 @@ janela.config(bg='#016064')
 janela.resizable(width=False, height=False)
 
 senha = []
+tratar_senha = ''
+
 
 def gerar_senha():
-  for i in range(3):                      
-    for _ in range(6):                    
-      senha.append(random.choice(string.digits + string.ascii_letters))
-    if i != 2:                            
-      senha.append('-')                  
-    else:                                 
-      senha.append('')      
-  
-  tratar_senha = "".join(senha)
-  nova_senha['text']= tratar_senha
-  senha.clear()
+    global tratar_senha
+    for i in range(3):
+        for _ in range(6):
+            senha.append(random.choice(string.digits + string.ascii_letters))
+        if i != 2:
+            senha.append('-')
+        else:
+            senha.append('')
 
-label = Label(janela, text='GERADOR DE SENHAS', bg='#016064', fg='white', font='Arial 10 bold', height=1, width=30)
+    tratar_senha = "".join(senha)
+    nova_senha['text'] = tratar_senha
+    senha.clear()
+
+
+def copiar_senha():
+    global tratar_senha
+    if tratar_senha == '':
+        pc.copy('Nenhuma senha foi criada ainda!')
+    else:
+        pc.copy(tratar_senha)
+
+
+label = Label(janela, text='GERADOR DE SENHAS', bg='#016064',
+              fg='white', font='Arial 10 bold', height=1, width=30)
 label.place(x=38, y=15)
 
-nova_senha = Label(janela, text='', bg='white', fg='black', font='Arial 14', height=2, width=23)
+nova_senha = Label(janela, text='', bg='white', fg='black',
+                   font='Arial 14', height=2, width=23)
 nova_senha.place(x=30, y=45)
 
-botao = Button(janela, text='Gerar senha', command=gerar_senha, bg='white', fg='black', relief='raised', height=1, width=10, font='Arial 10 bold')
-botao.place(x=120, y=105)
+botao = Button(janela, text='Gerar senha', command=gerar_senha, bg='white',
+               fg='black', relief='raised', height=1, width=10, font='Arial 10 bold')
+botao.place(x=60, y=105)
 
-
+botao = Button(janela, text='Copiar senha', command=copiar_senha, bg='white',
+               fg='black', relief='raised', height=1, width=10, font='Arial 10 bold')
+botao.place(x=170, y=105)
 
 
 janela.mainloop()
